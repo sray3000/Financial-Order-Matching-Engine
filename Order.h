2840ae966bbb72a8ec19a192aa588ff4c5060ae9
sray3000 <sraystark3000@gmail.com>
2026-08-06 11:22:07 +0530

    This commit adds the OrderBook and OrderMap entities

--- Order.h
+++ Order.h
@@ -1,5 +1,5 @@
 #pragma once
-#include<string>
+#include <string>
 
 struct Order {
     std::string orderId;
--- OrderBook.cc
+++ OrderBook.cc
@@ -1,20 +1,38 @@
-#include<iostream>
-#include<map>
-#include<queue>
-#include<functional>
-#include<first.h>
+#pragma once
+#include <iostream>
+#include <map>
+#include <list>
+#include <unordered_map>
+#include <functional>
+#include "Order.h"
 
 template<typename Comparator>
 class OrderBook {
 public:
-    std::map<long long, std::queue<Order>, Comparator> orderBook;
+    // Maps prices to list of orders by time
+    std::map<long long, std::list<Order>, Comparator> orderBook;
+    
+    // Maps orderId to its exact position in the list for O(1) cancellation
+    std::unordered_map<std::string, typename std::list<Order>::iterator> orderMap;
 
-    OrderBook() {
+    OrderBook() {};
 
-    };
-
-    void AddOrder(Order newOrder) {
+    void AddOrder(const Order& newOrder) {
         long long price = newOrder.price;
-        orderBook[price].push(newOrder);
+        orderBook[price].push_back(newOrder);
+        orderMap[newOrder.orderId] = std::prev(orderBook[price].end());
+    }
+
+    void CancelOrder(const std::string& orderId) {
+        auto it = orderMap.find(orderId);
+        if (it != orderMap.end()) {
+            long long price = it->second->price;
+            orderBook[price].erase(it->second);
+            orderMap.erase(it);
+            
+            if (orderBook[price].empty()) {
+                orderBook.erase(price);
+            }
+        }
     }
 };
