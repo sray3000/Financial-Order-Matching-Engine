4ab8b2de1b48a2be62ac7ef60ab1aa55b1ebe41e
sray3000 <sraystark3000@gmail.com>
2026-08-06 10:55:40 +0530

    This commit modifies the fields of an Order struct

---
+++ Order.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<string>
+
+struct Order {
+    std::string orderId;
+    bool type;              // 0 for bid, 1 for ask
+    long long price;
+    long long quantity;
+    std::string timestamp;
+};
