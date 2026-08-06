4888284b8b7e9115aa0a2c27fe539b2761444d92
sray3000 <sraystark3000@gmail.com>
2026-08-06 10:56:41 +0530

    This commit removes old redundant files

--- first.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-typedef struct Order {
-    std::string orderId;
-    bool type;      // 0 for bid. 1 for ask
-    long long price;
-    long long quantity;
-    std::string timestamp;
-} Order;
